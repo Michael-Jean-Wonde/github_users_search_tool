9a6c2afa23d49811b22006c02a4a1b64bef7de44
Michael-Jean-Wonde <michaeljeanwonde1839@gmail.com>
2022-07-06 00:07:08 +0300

    second commit

--- lib/func/data_source.dart
+++
@@ -1,8 +0,0 @@
-import 'package:github_search/func/base_network.dart';
-
-class UserDataSource {
-  static UserDataSource instance = UserDataSource();
-  Future<Map<String, dynamic>> loadUser(String username) {
-    return BaseNetwork.get("users/" + username);
-  }
-}
